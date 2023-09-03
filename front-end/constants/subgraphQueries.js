1884e88289076b4b9e053d3109a4808c74f1497b
Yannick Roy <ymyannick@gmail.com>
2023-09-03 08:07:49 +0700

    Update subgraphQueries.js
    Test solving listing bug.

--- front-end/constants/subgraphQueries.js
+++ front-end/constants/subgraphQueries.js
@@ -2,7 +2,7 @@ import { gql } from '@apollo/client'
 
 const GET_ACTIVE_ITEMS = gql`
     {
-        activeItems(first: 20, where: { buyer: "0x0000000000000000000000000000000000000000" }) {
+        activeItems(first: 20) {
             id
             buyer
             seller
