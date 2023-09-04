f89815d431d378f745737f823d44823d448901d5
Yannick Roy <ymyannick@gmail.com>
2023-09-04 23:11:26 +0800

    Update graphex.js
    Test (temp)

--- front-end/pages/graphex.js
+++ front-end/pages/graphex.js
@@ -2,9 +2,8 @@ import { useQuery, gql } from "@apollo/client";
 
 const GET_ACTIVE_ITEMS = gql`
   {
-    activeItems(
+    itemListeds(
       first: 5
-      where: { buyer: "0x0000000000000000000000000000000000000000" }
     ) {
       id
       buyer
