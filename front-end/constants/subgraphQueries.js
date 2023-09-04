4b22e826bf77e6e92b0f0895d40d6937f8d7ad7c
Yannick Roy <ymyannick@gmail.com>
2023-09-04 22:58:27 +0800

    Update subgraphQueries.js
    Typo fixed. (itemListeds with s)

--- front-end/constants/subgraphQueries.js
+++ front-end/constants/subgraphQueries.js
@@ -3,7 +3,7 @@ import { gql } from '@apollo/client'
 // Patch with itemListed but needs to bec activeItems. (otherwise you get ALL the listed, but coul be bought...)
 const GET_ACTIVE_ITEMS = gql`
     {
-        itemListed(first: 20) {
+        itemListeds(first: 20) {
             id
             buyer
             seller
