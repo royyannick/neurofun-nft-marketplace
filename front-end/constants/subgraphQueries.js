0d06b0679d31695097856e31b737054b101be03a
Yannick Roy <ymyannick@gmail.com>
2023-09-04 22:57:43 +0800

    Update subgraphQueries.js
    Patched (temporarily) the Graph Query to show stuff items on the MarketPlace

--- front-end/constants/subgraphQueries.js
+++ front-end/constants/subgraphQueries.js
@@ -1,8 +1,9 @@
 import { gql } from '@apollo/client'
 
+// Patch with itemListed but needs to bec activeItems. (otherwise you get ALL the listed, but coul be bought...)
 const GET_ACTIVE_ITEMS = gql`
     {
-        activeItems(first: 20) {
+        itemListed(first: 20) {
             id
             buyer
             seller
