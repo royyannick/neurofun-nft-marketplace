0598dd58d2fd3ced75efe0ae6f723a0fdbe9931c
Yannick Roy <ymyannick@gmail.com>
2023-09-04 23:03:40 +0800

    Update subgraphQueries.js
    Updated Query.

--- front-end/constants/subgraphQueries.js
+++ front-end/constants/subgraphQueries.js
@@ -3,14 +3,13 @@ import { gql } from '@apollo/client'
 // Patch with itemListed but needs to bec activeItems. (otherwise you get ALL the listed, but coul be bought...)
 const GET_ACTIVE_ITEMS = gql`
     {
-        itemListeds(first: 20) {
-            id
-            buyer
-            seller
-            nftAddress
-            tokenId
-            price
-        }
+      itemListeds(first: 20, orderBy: blockNumber, orderDirection: desc) {
+        id
+        nftAddress
+        price
+        seller
+        tokenId
+      }
     }
 `
 
